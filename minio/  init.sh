8038b1844421bf1e0063c24bc4a0e4be8081e613
ravepornsp <supapanchai_r@su.ac.th>
2026-03-24 20:11:15 +0700

    add minio

---
+++ minio/  init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+minio server /data --console-address ":9001" &
+sleep 5
+
+mc alias set local http://localhost:9000 minioadmin minioadmin123
+mc mb local/images
+mc anonymous set public local/images
+
+wait
