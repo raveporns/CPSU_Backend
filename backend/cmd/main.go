bd834b0ad7396b28ba57ca044e3dc9869b1c9874
ravepornsp <supapanchai_r@su.ac.th>
2026-03-24 23:18:34 +0700

    edit main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -153,8 +153,11 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"https://cpsu-website-beta.vercel.app",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
